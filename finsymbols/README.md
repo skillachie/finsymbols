572edfca4a855ed9cf2b98bf5c5c11ab70b77c90
Dwayne Campbell <dwaynecampbell13@gmail.com>
2013-08-11 11:00:23 -0400

    created setup file to be used with pypi

---
+++ finsymbols/README.md
@@ -0,0 +1,13 @@
+finsymbols
+==========
+
+Obtains stock symbols and relating information for SP500, AMEX, NYSE, and NASDAQ 
+
+
+
+*S&P 500 are obtained dynamically by parsing Wikipedia
+
+*AMEX,NYSE and NASDAQ are obtained by using downloaded CSV files.
+
+
+How to use  http://skillachie.github.io/finsymbols/
---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='finsymbols',
+    version='0.3.5',
+    packages=['finsymbols'],
+    package_dir={'finsymbols': 'finsymbols'},
+    package_data={'finsymbols':['exchanges/*.csv']},
+    include_package_data=True,
+    author='Dwayne V Campbell',
+    author_email='dwaynecampbell13 _at_ gmail.com',
+    description='Retrieves list of all symbols present in SP500, NASDAQ ,AMEX and NYSE',
+    long_description=open('README.md').read(),
+    url='http://skillachie.github.io/finsymbols/',
+    download_url='http://pypi.python.org/pypi/finsymbols',
+    keywords='stocks stockmarket yahoo finance SP500 NASDAQ AMEX NYSE'.split(),
+    license='GNU LGPLv2+',
+    install_requires=[
+        "beautifulsoup4 >= 4.2.1"
+                     ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Investment',
+      ]
+ )
