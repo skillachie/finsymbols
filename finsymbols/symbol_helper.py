028e0f96753568d9769ae0c5f0cba9044e5d1dd0
Gregory Rehm <grehm87@gmail.com>
2014-10-15 18:25:22 -0700

    Add method to grab company data more effectively using NASDAQ's API and add python3 support

--- finsymbols/symbol_helper.py
+++ finsymbols/symbol_helper.py
@@ -1,4 +1,7 @@
-import urllib2
+try:
+    import urllib2 as urllib
+except ImportError:  # python3
+    import urllib.request as urllib
 import os
 import sys
 import csv
@@ -6,15 +9,33 @@ import datetime
 import pprint
 
 
+def get_symbol_list(symbol_data):
+    symbol_list = list()
+    symbol_data = symbol_data.replace('"', "")
+    symbol_data = symbol_data.split("\r\n")
+    symbol_data = list(map(lambda x: x.split(","), symbol_data))
+    # We need to cut off the first row because it is the header and the last
+    # row because it is a null string
+    for row in symbol_data[1:-1]:
+        symbol_list.append(row[0])
+    return symbol_list
+
+
+def get_exchange_url(exchange):
+    return ("http://www.nasdaq.com/screening/companies-by-industry.aspx?"
+            "exchange={}&render=download".format(exchange))
+
+
 def is_connected():
     '''
     Checks internet connectivity
     '''
     try:
-        urllib2.urlopen("http://google.com", timeout=2)
+        urllib.urlopen("http://google.com", timeout=2)
         return True
-    except urllib2.URLError:
+    except urllib.URLError:
         return False
+
 
 def is_cached(file_path):
     '''
@@ -23,30 +44,30 @@ def is_cached(file_path):
     file_time = datetime.datetime.fromtimestamp(os.path.getctime(file_path))
     current_time = datetime.datetime.now()
     file_age = (current_time - file_time).total_seconds()
-    
+
     if(file_age > 86400 and is_connected()):
-        #print "The file is old"
         return False
     elif(is_connected()):
-        #print "The file is cached"
         return True
     else:
-        #print "Not connected to the internet using last obtained file"
         return True
-
 
 
 def fetch_file(url):
-        '''
-        Gets and downloads files
-        '''
-        file_fetcher = urllib2.build_opener()
-        file_fetcher.addheaders =  [('User-agent', 'Mozilla/5.0')]
-        file_data = file_fetcher.open(url).read()
+    '''
+    Gets and downloads files
+    '''
+    file_fetcher = urllib.build_opener()
+    file_fetcher.addheaders =  [('User-agent', 'Mozilla/5.0')]
+    file_data = file_fetcher.open(url).read()
+    if isinstance(file_data, bytes): # Python3
+        return file_data.decode("utf-8")
+    elif isinstance(file_data, str):
         return file_data
 
+
 def wiki_html(url,file_name):
-    ''' 
+    '''
     Obtains html from Wikipedia
     Note: API exist but for my use case. Data returned was not parsable. Preferred to use html
     python-wikitools - http://code.google.com/p/python-wikitools/
@@ -55,9 +76,9 @@ def wiki_html(url,file_name):
     module_path = os.path.dirname(os.path.realpath(__file__))
     exchanges_dir = os.path.join(module_path,'exchanges')
     file_path = os.path.abspath(exchanges_dir) + os.sep + file_name
-   
+
     if(os.path.exists(file_path) and is_cached(file_path)):
-        opener = urllib2.build_opener()
+        opener = urllib.build_opener()
         return opener.open("file://" + file_path).read()
     else:
         wiki_html = fetch_file('http://en.wikipedia.org/wiki/'+str(url))
@@ -70,13 +91,13 @@ def wiki_html(url,file_name):
 def get_csv(exchange):
     '''
     Downloads the symbol list for each exchange from Nasdaq
-    '''    
+    '''
     module_path = os.path.dirname(os.path.realpath(__file__))
     exchanges_dir = os.path.join(module_path,'exchanges')
     file_path = os.path.abspath(exchanges_dir) + os.sep + exchange + '.csv'
-    
+
     if(os.path.exists(file_path) and is_cached(file_path)):
-        return file_path 
+        return file_path
     else:
         csv_file = fetch_file('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange='+str(exchange)+'&render=download')
         saved_file = open(file_path , "w")
@@ -84,19 +105,18 @@ def get_csv(exchange):
         saved_file.close()
         return file_path
 
+
 def read_csv(exchange):
-    '''  
+    '''
     Reads symbols in csv file as dict
     '''
     symbol_list = list()
     file_path = get_csv(exchange)
     exchange_file = csv.DictReader(open(file_path),fieldnames=['symbol', 'company', 'LastSale', 'MarketCap', 'ADR TSO', 'IPOyear', 'sector', 'industry', 'Summary Quote', ''])
     next(exchange_file, None)
-	
+
     for row in exchange_file:
         row['symbol'] = row['symbol'].encode('utf-8').strip()
         symbol_list.append(row)
 
-    return symbol_list	
-
-
+    return symbol_list
