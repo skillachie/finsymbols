a8065457d38ddc08d5d148f19bd5fc808ae43b7c
Wessel dR <j.w.deroode@gmail.com>
2016-09-05 01:32:39 +0200

    Update symbols_test.py

--- finsymbols/tests/symbols_test.py
+++ finsymbols/tests/symbols_test.py
@@ -11,7 +11,7 @@ class TestSizeOfList(TestCase):
     def test_sp500_size(self):
         sp500 = symbols.get_sp500_symbols()
         assert len(sp500) == 504, 'len gathered data: {}.\
-         Expected len: 504'.format(len(sp500))
+         Expected len: 505'.format(len(sp500))
 
     def test_amex_not_null(self):
         amex = symbols.get_amex_symbols()
